6fb2da00a4dc1391bbbd5099f909853fbad48ed2
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-09-22 14:56:23 -0400

    Lab Completed

--- server/app.py
+++ server/app.py
@@ -3,3 +3,13 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+
